3422e2174cdd25527d22c5ebd7d28f438f00430e
anribras <bravo911@163.com>
2020-11-29 00:32:35 +0800

    sql works

--- web_service/blueprints/public.py
+++ web_service/blueprints/public.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, url_for
 import json
+from ..model.mariadb import User,db
 
 bp = Blueprint('public', __name__)
 
@@ -11,4 +12,8 @@ def index():
 
 @bp.route('/activity')
 def activity_controller():
+    user = User(name="yang")
+    db.session.add(user)
+    db.session.commit()
+
     return url_for('.activity_controller', host='666')
--- web_service/model/mariadb.py
+++ web_service/model/mariadb.py
@@ -6,3 +6,8 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128))
+
+class Activity(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(512))
+    intervals = db.Column(db.BigInteger())
