fdd1650c6b85af0561f148cbe760c7a862d14723
anribras <bravo911@163.com>
2020-11-30 22:49:42 +0800

    data model and api go

--- web_service/blueprints/public.py
+++ web_service/blueprints/public.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, request, url_for
+from flask import Blueprint, request, url_for, make_response, jsonify
 import json
-from ..model.mariadb import db
+from ..model.mariadb import db, Activity, Voice, Url
 
 bp = Blueprint('public', __name__)
 
@@ -10,11 +10,32 @@ def index():
     return json.dumps({'a': 'a', 'b': 'b'})
 
 
-@bp.route('/activity')
+@bp.route('/activities', methods=['POST'])
 def activity_controller():
-    name = request.args.get('name', '')
+    name = request.form.get('name', '')
+    types = request.form.get('type', '')
+    user = request.form.get('user', '')
+    intervals = request.form.get('user', 30)
+    current_longtitude = request.form.get('current_longtitude', 0)
+    current_tatitude = request.form.get('current_latitude', 0)
+    end_longtitude = request.form.get('end_longtitude', 0)
+    end_tatitude = request.form.get('end_latitude', 0)
 
-    # db.session.add(user)
+    activity = Activity(user, name, intervals, type, current_longtitude,
+                        current_tatitude, end_longtitude, end_tatitude)
+
+    db.session.add(activity)
+    db.session.flush()
     db.session.commit()
 
-    return url_for('.activity_controller', host='666')
+    response = make_response(jsonify(
+        {
+            'url': request.host_url + '666',
+            'activity_id': activity.id
+        })
+    )
+
+    response.headers['ContentType'] = 'application/json'
+    response.status_code = 200
+
+    return response
--- web_service/model/mariadb.py
+++ web_service/model/mariadb.py
@@ -1,7 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
+from web_service.helper.utils import get_short_code
 db = SQLAlchemy()
+
 
 
 # class User(db.Model):
@@ -17,22 +18,22 @@ class TimeStampMixin(object):
 class Activity(TimeStampMixin, db.Model):
     __tablename__ = 'activities'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user = db.Column(db.String(20), default='')
+    atype = db.Column(db.Integer, default=0)
     name = db.Column(db.String(512), default='')
     intervals = db.Column(db.Integer, default=60)
-    types = db.Column(db.Integer, default=0)
     current_longtitude = db.Column(db.FLOAT)
     current_latitude = db.Column(db.FLOAT)
     end_longtitude = db.Column(db.FLOAT, nullable=True)
     end_latitude = db.Column(db.FLOAT, nullable=True)
 
-    def __init__(self, user, name, intervals, types, current_longtitude,
+    def __init__(self, user, name, intervals, atype, current_longtitude,
                  current_latitude, end_longtitude, end_latitude):
         self.user = user
         self.name = name
         self.intervals = intervals
-        self.types = types
+        self.atypes = atype
         self.current_longtitude = current_longtitude
         self.current_latitude = current_latitude
         self.end_longtitude = end_longtitude
@@ -45,7 +46,7 @@ class Activity(TimeStampMixin, db.Model):
 class Voice(TimeStampMixin, db.Model):
     __tablename__ = 'voices'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
     name = db.Column(db.String(500))
     size = db.Column(db.Integer)
     md5 = db.Column(db.String(128))
@@ -60,11 +61,24 @@ class Voice(TimeStampMixin, db.Model):
         self.activity_id = activity_id
 
 
+
 class Url(TimeStampMixin, db.Model):
     __tablename__ = 'urls'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
     hash_code = db.Column(db.String(128))
+    activity_id = db.Column(db.Integer)
 
-    def __init__(self, hash_code):
+    def __init__(self, hash_code, activity_id):
         self.hash_code = hash_code
+        self.activity_id = activity_id
+
+# modify this in migration/env.py:
+    # with connectable.connect() as connection:
+    #     context.configure(
+    #         connection=connection,
+    #         target_metadata=target_metadata,
+    #         process_revision_directives=process_revision_directives,
+    #         render_as_batch=True, # this line
+    #         **current_app.extensions['migrate'].configure_args
+    #     )
